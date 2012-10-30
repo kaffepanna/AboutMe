b836792769731ee3ee537ff5346367c6b4a0a752
unknown <patrik.pettersson@KNPAP.kndomain.local>
2012-10-30 08:03:48 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AboutMe::Application.config.secret_token = '712c508ea02a9c8f2f93f631154fccbe30e2fe78a43ecda6ff15efbe33b055f7bd0cc62de8ff2df4f2d21ba58258f4bf74a48d7e0680f826c1f09c19be892f16'
