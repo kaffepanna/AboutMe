e2055be58c58a0fa8ab0185d29247bbffa10197c
Patrik Pettersson <pettersson.pa@gmail.com>
2012-10-31 13:07:57 +0100

    Typo in log truncation

--- config/initializers/sql_log_trunkater.rb
+++ config/initializers/sql_log_trunkater.rb
@@ -3,7 +3,7 @@ class ActiveRecord::ConnectionAdapters::AbstractAdapter
 
   def log_with_trunkate(sql, name="SQL", binds=[], &block)
     b = binds.map {|k,v|
-      v = v.truncate(50) if v.is_a? String and v.size > 20
+      v = v.truncate(20) if v.is_a? String and v.size > 20
       [k,v]
     }
     log_without_trunkate(sql, name, b, &block)
