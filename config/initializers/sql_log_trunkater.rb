967a418456bff70726368008dcd4f12efb5b1342
Patrik Pettersson <pettersson.pa@gmail.com>
2012-10-31 12:59:09 +0100

    sql log trunkation

---
+++ config/initializers/sql_log_trunkater.rb
@@ -0,0 +1,14 @@
+class ActiveRecord::ConnectionAdapters::AbstractAdapter
+  protected
+
+  def log_with_trunkate(sql, name="SQL", binds=[], &block)
+    b = binds.map {|k,v|
+      v = v.truncate(50) if v.is_a? String and v.size > 20
+      [k,v]
+    }
+    log_without_trunkate(sql, name, b, &block)
+  end
+
+  alias_method_chain :log, :trunkate
+end
+
