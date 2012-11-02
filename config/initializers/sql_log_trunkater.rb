0e8337ce3009972911a64157eaa642ee35adda09
Patrik Pettersson <pettersson.pa@gmail.com>
2012-11-02 13:47:53 +0100

    Fixed log truncation
    Fixed log truncation for sql updates

--- config/initializers/sql_log_trunkater.rb
+++ config/initializers/sql_log_trunkater.rb
@@ -2,11 +2,11 @@ class ActiveRecord::ConnectionAdapters::AbstractAdapter
   protected
 
   def log_with_trunkate(sql, name="SQL", binds=[], &block)
-    b = binds.map {|k,v|
+    b = binds.map {|k,v| 
       v = v.truncate(20) if v.is_a? String and v.size > 20
       [k,v]
     }
-    s = sql.size > 50 ? sql.truncate(50) : sql
+    s = sql.gsub(/x\'[A-Za-z0-9]+\'/) { |match| match.size > 20 ? "'...'" : match }
     log_without_trunkate(s, name, b, &block)
   end
 
