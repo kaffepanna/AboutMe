1f515a4551c4fac05885da0b6a1b832ac0d4f1c3
Patrik Pettersson <pettersson.pa@gmail.com>
2012-11-06 19:12:24 +0100

    fixed login/logout button

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
+  acts_as_authentic do |c|
+    c.login_field :email
+  end
   attr_accessible :email, :login, :password, :password_confirmation
 end
